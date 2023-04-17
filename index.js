a255f6367674d5e7dc54cddf2387e4dc37b6533c
Andrey Kurnosov <beezdar123455@gmail.com>
2023-04-18 01:09:24 +0300

    converter

--- index.js
+++ index.js
@@ -1,58 +1,66 @@
 "use strict";
 
-const deadLine = "2023-12-31";
-
-const title = document.querySelector('h3');
-
-title.textContent = `${deadLine} dl`;
-
-const getTime = (endtime) => {
-	const total = Date.parse(endtime) - Date.parse(new Date());
-	const days = Math.floor(total / (1000 * 60 * 60 * 24));
-	const hourse = Math.floor(total / ( 1000 * 60 * 60) %  24);
-	const minutes = Math.floor(total / (1000 * 60) % 60);
-	const seconds = Math.floor((total / 1000) % 60);
-	return {
-		total,
-		days,
-		hourse,
-		minutes,
-		seconds,
-	};
+const data = {
+	euro: 89.64,
+	usd: 81.55,
 };
 
-function getZero(num) {
-	if (num < 10) {
-		return `0${num}`;
+let valueOne = 'rub';
+let valueTwo = 'usd';
+
+const oneInput = document.getElementById('one');
+const twoInput = document.getElementById('two');
+const selectOne = document.querySelector('.list-one');
+const selectTwo = document.querySelector('.list-two');
+
+const calcConverter = (selectOne, selectTwo, value) => {
+	if (selectOne === 'rub' && selectTwo === 'rub') {
+		twoInput.value = value;
 	}
-	return num;
-}
+	if (selectOne === 'rub' && selectTwo === 'usd') {
+		twoInput.value = (value * data.usd).toFixed(2);
+	}
+	if (selectOne === 'rub' && selectTwo === 'euro') {
+		twoInput.value = (value * data.euro).toFixed(2);
+	}
 
+	if (selectOne === 'usd' && selectTwo === 'usd') {
+		twoInput.value = value;
+	}
+	if (selectOne === 'usd' && selectTwo === 'rub') {
+		twoInput.value = (value * data.usd).toFixed(2);
+	}
+	if (selectOne === 'usd' && selectTwo === 'euro') {
+		twoInput.value = ((value * data.usd) / data.euro).toFixed(2);
+	}
 
-const setClock = (endtime) => {
-	const days = document.getElementById('days');
-	const hourse = document.getElementById('hourse');
-	const minutes = document.getElementById('min');
-	const seconds = document.getElementById('sec');
-	const timerId = setInterval(updateClock, 1000);
-
-	updateClock();
-
-	function updateClock() {
-		const t = getTime(endtime);
-		days.innerHTML = getZero(t.days);
-		hourse.innerHTML = getZero(t.hourse);
-		minutes.innerHTML = getZero(t.minutes);
-		seconds.innerHTML = getZero(t.seconds);
-		if (t.total <= 0) {
-			clearInterval(timerId);
-		}
+	if (selectOne === 'euro' && selectTwo === 'euro') {
+		twoInput.value = value;
+	}
+	if (selectOne === 'euro' && selectTwo === 'rub') {
+		twoInput.value = (value * data.euro).toFixed(2);
+	}
+	if (selectOne === 'euro' && selectTwo === 'usd') {
+		twoInput.value = ((value * data.euro) / data.usd).toFixed(2);
 	}
 };
 
-setClock(deadLine);
+calcConverter(valueOne, valueTwo, oneInput.value);
+
+selectOne.onchange = (e) => {
+	let i = selectOne.value;
+	valueOne = i;
+	calcConverter(valueOne, valueTwo, oneInput.value);
+};
+selectTwo.onchange = (e) => {
+	let i = selectTwo.value;
+	valueTwo = i;
+	calcConverter(valueOne, valueTwo, oneInput.value);
+};
 
 
-
-
-// console.log(getTime(deadLine));
+oneInput.oninput = (e) => {
+	if (e.target.value === '') return;
+	const value = e.target.value;
+	calcConverter(valueOne, valueTwo, value);
+};
